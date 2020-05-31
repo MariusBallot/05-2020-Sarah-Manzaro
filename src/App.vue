6c0a72fced7e89a04004d5e0462208329cd41288
marius ballot <ballot.marius@gmail.com>
2020-05-31 10:32:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <ComingSoon msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import ComingSoon from "./components/ComingSoon.vue";
+
+export default {
+  name: "App",
+  components: {
+    ComingSoon
+  }
+};
+</script>
+
+<style lang="stylus">
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500&family=Raleway:wght@400;700&display=swap');
+
+body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="comingSoon">
+    <h1>- site en travaux</h1>
+    <!-- <p>
+      Hello toi ! Mon portfolio est en travaux, il sera disponible prochainement
+      tout beau et tout neuf ! En attendant tu peux jeter un coup d’oeil à mon portfolio et
+      mon CV ci-dessous si t’es curieux·se :)
+    </p>
+    <p>
+      Au passage je cherche une alternance pour les deux
+      prochaines années pour le master Design et management de l’innovation interactive «lead créatif»,
+      donc n’hésite pas à me contacter !
+    </p>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComingSoon",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+.comingSoon {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: repeat(7, 1fr);
+  grid-template-columns: repeat(12, 1fr);
+
+  h1 {
+    grid-column-start: 2;
+    grid-column-end: 6;
+    grid-row-start: 2;
+    grid-row-end: 3;
+    justify-self: center;
+    align-self: center;
+  }
+}
+</style>
